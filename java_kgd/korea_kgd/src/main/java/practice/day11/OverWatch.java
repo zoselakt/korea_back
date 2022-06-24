2ec433c75a9e266e2e1e33eec1cc8425933907f6
zoselakt <kd66@naver.com>
2022-06-24 17:26:36 +0900

    sixth_commit

---
+++ java_kgd/korea_kgd/src/main/java/practice/day11/OverWatch.java
@@ -0,0 +1,10 @@
+package practice.day11;
+
+public interface OverWatch {
+	void name();
+	void lClick();
+	void rClick(); 
+	void shiftButton(); 
+	void eButton();
+	void qButton();
+}
---
+++ java_kgd/korea_kgd/src/main/java/practice/day11/OverWatchProject.java
@@ -0,0 +1,27 @@
+package practice.day11;
+
+import java.util.Scanner;
+
+public class OverWatchProject {
+	public static void main(String[] args) {
+		OverWatch overWatch;
+		System.out.println("1. 메이 | 2. 리퍼 | 3. 맥크리");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		if(n == 1){
+			overWatch = new Mei();
+		}else if(n == 2) {
+			overWatch = new Reaper();
+		}else {
+			overWatch = new Mecree();
+		}
+		
+		overWatch.name();
+		overWatch.lClick();
+		overWatch.rClick();
+		overWatch.shiftButton();
+		overWatch.eButton();
+		overWatch.qButton();
+	}
+}
