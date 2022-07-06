3d0867b9d80fa2e61c39717285033ed437c0d80f
zoselakt <kd66@naver.com>
2022-07-06 16:57:12 +0900

    thirteenth_commit

---
+++ jv301/src/main/resources/jv251.sql
@@ -0,0 +1,14 @@
+create table addmember(
+	num bigint	primary key auto_increment,
+	id	varchar(16) not null,
+	password varchar(16) not null,
+	name varchar(20) not null,
+	ssn varchar(14) not null,
+	email1 varchar(11) not null,
+	email2 varchar(11) not null,
+	addr1 varchar(20) not null,
+	addr2 varchar(20) not null,
+	regData timestamp not null default current_timestamp
+) auto_increment = 1;
+
+select * from addmember;
--- jv301/src/main/webapp/mod009/list_course_contents.jsp
+++ jv301/src/main/webapp/mod009/list_course_contents.jsp
@@ -1,6 +1,7 @@
+<%@ page import="com.varxyz.jv300.mod009.InitializeDataSource"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
     <%@ page import = "java.util.*" %>
 <!DOCTYPE html>
 <html>
